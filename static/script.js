bb5a53a79f5984544522e59bc744c95b57660b06
Jose Pérez Cano <joseperez2000@hotmail.es>
2021-08-28 20:40:42 +0200

    Security fix

--- static/script.js
+++ static/script.js
@@ -52,7 +52,7 @@ function checkLog(){
   }, function(data, status){
     if (data === 'True'){
       if (document.getElementById("user").value === 'admin'){
-        $.get('/c7ad44cbad762a5da0a452f9e854fdc1e0e7a52a38015f23f3eab1d80b931dd472634dfac71cd34ebc35d16ab7fb8a90c81f975113d6c7538dc69dd8de9077ec/', function(data, status){
+        $.get('/c7ad44cbad762a5da0a452/', function(data, status){
           document.write(data);
         });
       } else {
